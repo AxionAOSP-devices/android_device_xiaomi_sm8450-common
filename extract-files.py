38dda6dd2854fe9316c093fac68e51b03a76953c
beingashwani <ashwanic177@gmail.com>
2025-08-06 06:09:15 +0000

    Revert "sm8450-common: Add support for Dolby Vision"
    This reverts commit 5eda3ec37f71ed2e3e2d40cd0ac1f0459b766d39.

--- extract-files.py
+++ extract-files.py
@@ -108,12 +108,6 @@ blob_fixups: blob_fixups_user_type = {
         .regex_replace('.+dolby.+\n', ''),
     'vendor/lib64/vendor.libdpmframework.so': blob_fixup()
         .add_needed('libhidlbase_shim.so'),
-        ('vendor/lib/c2.dolby.avc.dec.so', 'vendor/lib/c2.dolby.avc.sec.dec.so', 'vendor/lib/c2.dolby.hevc.dec.so', 'vendor/lib/c2.dolby.hevc.sec.dec.so'): blob_fixup()
-        .add_needed('libstagefright_foundation-v33.so'),
-        ('vendor/bin/hw/dolbycodec2'): blob_fixup()
-        .add_needed('libstagefright_foundation-v33.so'),
-        ('vendor/lib/c2.dolby.client.so'): blob_fixup()
-        .add_needed('libcodec2_hidl_shim.so'),
 }  # fmt: skip
 
 module = ExtractUtilsModule(
